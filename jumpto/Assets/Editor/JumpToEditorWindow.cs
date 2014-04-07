1648750385c48f34fc896ea25e74bc21271d3bf4
dean <dean>
2014-04-07 04:29:26 +0000

    made the lists look nice, but it needs work. see ILSpy, UnityEditor.BaseProjectWindow.HierarchyView() for reference.

--- jumpto/Assets/Editor/JumpToEditorWindow.cs
+++ jumpto/Assets/Editor/JumpToEditorWindow.cs
@@ -4,8 +4,8 @@ using System.Collections.Generic;
 
 
 //TODO: make items clickable (what happens when clicked?)
-//TODO: draw hierarchy list correclty
-//TODO: project and hierarchy foldouts
+//xTODO: project and hierarchy foldouts
+//TODO: replace GUILayout.Label with m_LinkLabelStyle.Draw()
 //TODO: ability to remove items
 //TODO: context menu
 //TODO: drag-n-drop to/from window
@@ -16,73 +16,75 @@ using System.Collections.Generic;
 
 public class JumpToEditorWindow : EditorWindow
 {
-	[SerializeField]
-	private List<UnityEngine.Object> m_ProjectReferences = new List<Object>();
+	public class LinkedObject
+	{
+		public UnityEngine.Object LinkReference;
+		public GUIContent LinkLabelContent = new GUIContent();
+		public Color LinkColor = Color.black;
+		public System.Type LinkType = null;
+	}
+
 
 	[SerializeField]
-	private List<UnityEngine.Object> m_HierarchyReferences = new List<Object>();
-	//private SerializedObject m_SerializedObject;
+	private List<LinkedObject> m_ProjectReferences = new List<LinkedObject>();
+
+	[SerializeField]
+	private List<LinkedObject> m_HierarchyReferences = new List<LinkedObject>();
+
+	private bool m_ProjectLinksUnfolded = true;
+	private bool m_HierarchyLinksUnfolded = true;
 
 	private Texture2D m_IconPrefabNormal;
 	private Texture2D m_IconPrefabModel;
+	private Texture2D m_IconGameObject;
+
+	private GUIStyle  m_LinkLabelStyle;
+
+	private Color ColorViolet = new Color(0.6f, 0.27f, 0.67f, 1.0f);
 
 
 	void OnEnable()
 	{
 		m_IconPrefabNormal = EditorGUIUtility.FindTexture("PrefabNormal Icon");
 		m_IconPrefabModel = EditorGUIUtility.FindTexture("PrefabModel Icon");
+		m_IconGameObject = EditorGUIUtility.FindTexture("GameObject Icon");
 	}
 
 	void OnGUI()
 	{
-		GUIContent objContent;
-		GUILayout.Label("Project References");
-		for (int i = 0; i < m_ProjectReferences.Count; i++)
+		//NOTE: it's ridiculous that I have to do this here.
+		if (m_LinkLabelStyle == null)
 		{
-			//EditorGUILayout.ObjectField(m_ProjectReferences[i], m_ProjectReferences[i].GetType(), true);
-			objContent = EditorGUIUtility.ObjectContent(m_ProjectReferences[i], m_ProjectReferences[i].GetType());
-			if (m_ProjectReferences[i] is GameObject)
-			{
-				if (objContent.image != null)
-				{
-					Debug.Log(AssetDatabase.GetAssetPath(objContent.image.GetInstanceID()));
-				}
-				else
-				{
-					PrefabType prefabType = PrefabUtility.GetPrefabType(m_ProjectReferences[i]);
-					if (prefabType == PrefabType.Prefab)
-						objContent.image = m_IconPrefabNormal;
-					else if (prefabType == PrefabType.ModelPrefab)
-						objContent.image = m_IconPrefabModel;
-				}
-			}
+			m_LinkLabelStyle = new GUIStyle(GUI.skin.label);
+			m_LinkLabelStyle.padding.left = 17;
+		}
 
-			if (objContent.text == string.Empty)
+		m_ProjectLinksUnfolded = EditorGUILayout.Foldout(m_ProjectLinksUnfolded, "Project References");
+		if (m_ProjectLinksUnfolded)
+		{
+			for (int i = 0; i < m_ProjectReferences.Count; i++)
 			{
-				if (m_ProjectReferences[i].name != string.Empty)
-				{
-					objContent.text = m_ProjectReferences[i].name;
-				}
-				else
-				{
-					string assetName = AssetDatabase.GetAssetPath(m_ProjectReferences[i].GetInstanceID());
-					int slash = assetName.LastIndexOf('/');
-					int dot = assetName.LastIndexOf('.');
-					objContent.text = assetName.Substring(slash + 1, dot - slash - 1);
-				}
+				GUILayout.BeginHorizontal();
+				m_LinkLabelStyle.normal.textColor = m_ProjectReferences[i].LinkColor;
+				GUILayout.Space(17.0f);
+				GUILayout.Label(m_ProjectReferences[i].LinkLabelContent, m_LinkLabelStyle, GUILayout.Height(16.0f));
+				GUILayout.EndHorizontal();
 			}
-
-			GUILayout.BeginHorizontal();
-			GUILayout.Label(objContent, GUILayout.Height(16.0f));
-			GUILayout.EndHorizontal();
 		}
 
 		EditorGUILayout.Separator();
 
-		GUILayout.Label("Hierarchy References");
-		for (int i = 0; i < m_HierarchyReferences.Count; i++)
+		m_HierarchyLinksUnfolded = EditorGUILayout.Foldout(m_HierarchyLinksUnfolded, "Hierarchy References");
+		if (m_HierarchyLinksUnfolded)
 		{
-			EditorGUILayout.ObjectField(m_HierarchyReferences[i], m_HierarchyReferences[i].GetType(), true);
+			for (int i = 0; i < m_HierarchyReferences.Count; i++)
+			{
+				GUILayout.BeginHorizontal();
+				m_LinkLabelStyle.normal.textColor = m_HierarchyReferences[i].LinkColor;
+				GUILayout.Space(17.0f);
+				GUILayout.Label(m_HierarchyReferences[i].LinkLabelContent, m_LinkLabelStyle, GUILayout.Height(16.0f));
+				GUILayout.EndHorizontal();
+			}
 		}
 	}
 
@@ -95,6 +97,7 @@ public class JumpToEditorWindow : EditorWindow
 	//***** Only called if focused *****
 	void OnHierarchyChange()
 	{
+		Debug.Log("Hierarchy changed");
 		CheckHierarchyLinks();
 		Repaint();
 	}
@@ -118,16 +121,16 @@ public class JumpToEditorWindow : EditorWindow
 				prefabType == PrefabType.DisconnectedPrefabInstance ||
 				prefabType == PrefabType.DisconnectedModelPrefabInstance)
 			{
-				AddHierarchyLink(linkObject);
+				AddHierarchyLink(linkObject, prefabType);
 			}
 			else
 			{
-				AddProjectLink(linkObject);
+				AddProjectLink(linkObject, prefabType);
 			}
 		}
 		else
 		{
-			AddProjectLink(linkObject);
+			AddProjectLink(linkObject, prefabType);
 		}
 	}
 
@@ -139,16 +142,117 @@ public class JumpToEditorWindow : EditorWindow
 		}
 	}
 
-	private void AddHierarchyLink(UnityEngine.Object linkObject)
+	private void AddHierarchyLink(UnityEngine.Object linkObject, PrefabType prefabType)
 	{
-		if (!m_HierarchyReferences.Contains(linkObject))
-			m_HierarchyReferences.Add(linkObject);
+		//basically, if no linked object in the list has a reference to the passed object
+		if (!m_HierarchyReferences.Exists(linked => linked.LinkReference == linkObject))
+		{
+			LinkedObject link = new LinkedObject();
+			link.LinkReference = linkObject;
+			link.LinkType = linkObject.GetType();
+			GUIContent linkContent = EditorGUIUtility.ObjectContent(linkObject, link.LinkType);
+			link.LinkLabelContent.text = linkContent.text != string.Empty ? linkContent.text : "[Unnamed]";
+			link.LinkLabelContent.image = linkContent.image;
+			
+			if (linkObject is GameObject)
+			{
+				if (prefabType == PrefabType.PrefabInstance)
+				{
+					if (link.LinkLabelContent.image == null)
+						link.LinkLabelContent.image = m_IconPrefabNormal;
+
+					link.LinkColor = Color.blue;
+				}
+				else if (prefabType == PrefabType.ModelPrefabInstance)
+				{
+					if (link.LinkLabelContent.image == null)
+						link.LinkLabelContent.image = m_IconPrefabModel;
+					link.LinkColor = ColorViolet;
+				}
+				else if (prefabType == PrefabType.DisconnectedPrefabInstance)
+				{
+					if (link.LinkLabelContent.image == null)
+						link.LinkLabelContent.image = m_IconPrefabNormal;
+					
+					link.LinkColor = Color.red;
+				}
+				else if (prefabType == PrefabType.DisconnectedModelPrefabInstance)
+				{
+					if (link.LinkLabelContent.image == null)
+						link.LinkLabelContent.image = m_IconPrefabModel;
+
+					link.LinkColor = Color.red;
+				}
+				else if (link.LinkLabelContent.image == null)
+				{
+					link.LinkLabelContent.image = m_IconGameObject;
+					//color = black
+				}
+
+				Transform linkTransform = (linkObject as GameObject).transform;
+				link.LinkLabelContent.tooltip = GetTransformPath(linkTransform);
+			}
+
+			m_HierarchyReferences.Add(link);
+		}
 	}
 
-	private void AddProjectLink(UnityEngine.Object linkObject)
+	private void AddProjectLink(UnityEngine.Object linkObject, PrefabType prefabType)
 	{
-		if (!m_ProjectReferences.Contains(linkObject))
-			m_ProjectReferences.Add(linkObject);
+		//basically, if no linked object in the list has a reference to the passed object
+		if (!m_ProjectReferences.Exists(linked => linked.LinkReference == linkObject))
+		{
+			LinkedObject link = new LinkedObject();
+			link.LinkReference = linkObject;
+			link.LinkType = linkObject.GetType();
+
+			GUIContent linkContent = EditorGUIUtility.ObjectContent(linkObject, link.LinkType);
+			link.LinkLabelContent.image = linkContent.image;
+			link.LinkLabelContent.tooltip = AssetDatabase.GetAssetPath(linkObject);
+
+			if (linkContent.text == string.Empty)
+			{
+				if (linkObject.name != string.Empty)
+				{
+					link.LinkLabelContent.text = linkObject.name;
+				}
+				else
+				{
+					string assetName = AssetDatabase.GetAssetPath(linkObject.GetInstanceID());
+					int slash = assetName.LastIndexOf('/');
+					int dot = assetName.LastIndexOf('.');
+					link.LinkLabelContent.text = assetName.Substring(slash + 1, dot - slash - 1);
+				}
+			}
+			else
+			{
+				link.LinkLabelContent.text = linkContent.text;
+			}
+
+			if (linkObject is GameObject)
+			{
+				if (prefabType == PrefabType.Prefab)
+				{
+					if (link.LinkLabelContent.image == null)
+						link.LinkLabelContent.image = m_IconPrefabNormal;
+
+					link.LinkColor = Color.blue;
+				}
+				else if (prefabType == PrefabType.ModelPrefab)
+				{
+					if (link.LinkLabelContent.image == null)
+						link.LinkLabelContent.image = m_IconPrefabModel;
+					link.LinkColor = ColorViolet;
+				}
+				else if (link.LinkLabelContent.image == null)
+				{
+					link.LinkLabelContent.image = m_IconGameObject;
+					//color = black
+				}
+			}
+
+			m_ProjectReferences.Add(link);
+		}
 	}
 
 	private void CheckHierarchyLinks()
@@ -161,6 +265,18 @@ public class JumpToEditorWindow : EditorWindow
 		//TODO: how?
 	}
 
+	private string GetTransformPath(Transform transform)
+	{
+		string path = string.Empty;
+		while (transform != null)
+		{
+			path = "/" + transform.name + path;
+			transform = transform.parent;
+		}
+
+		return path;
+	}
+	
 	[MenuItem("Tools/Jump To")]
 	public static void JumpTo_InitMainMenu()
 	{
